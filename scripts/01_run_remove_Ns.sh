bec6f7101f94ee32f523d02d7aef3b4c557dedd3
khmaher <katymaheris@gmail.com>
2025-11-11 14:28:05 +0000

    Update 01_run_remove_Ns.sh

--- scripts/01_run_remove_Ns.sh
+++ scripts/01_run_remove_Ns.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH -A molecolb
-#SBATCH -p molecolb
+##SBATCH -A molecolb
+##SBATCH -p molecolb
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --time=24:00:00
 
@@ -49,6 +49,6 @@ if [ "$marker" ]; then ARGS="$ARGS -C $marker"; fi
 
 ## load R and call Rscript
 source ~/.bash_profile
-conda activate /usr/local/extras/Genomics/apps/mambaforge/envs/metabarcoding
+conda activate metabarcoding
 echo $ARGS
 Rscript $PWD/scripts/01_remove_Ns.R $ARGS 
