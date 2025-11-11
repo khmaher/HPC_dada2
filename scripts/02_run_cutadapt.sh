90fd939b2c7e70ea7e99d3517d99195f574da10f
khmaher <katymaheris@gmail.com>
2025-11-11 14:35:16 +0000

    Update 02_run_cutadapt.sh

--- scripts/02_run_cutadapt.sh
+++ scripts/02_run_cutadapt.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH -A molecolb
-#SBATCH -p molecolb
+##SBATCH -A molecolb
+##SBATCH -p molecolb
 #SBATCH --mem-per-cpu=16GB
 #SBATCH --time=24:00:00
 
@@ -70,5 +70,5 @@ if [ "$marker" ]; then ARGS="$ARGS -C $marker"; fi
 
 ## load R and call Rscript
 source ~/.bash_profile
-conda activate /usr/local/extras/Genomics/apps/mambaforge/envs/metabarcoding
+conda activate metabarcoding
 Rscript $PWD/scripts/02_cutadapt.R $ARGS
